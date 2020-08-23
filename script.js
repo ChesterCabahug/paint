68addf70c3271174bbbf30032b6cccd32f489509
Chester <chesterdeveloper2000@gmail.com>
2020-08-23 15:15:22 +0800

    >> when changing the background of the canvas, the entire drawing gets cleared. also, the clear btn doesn't work at the moment
    uncommented lines 150 -158
    --with it, it will store your drawing data in an array called drawnArray.
    --it's the storeDrawn() that will take care of that
    
    uncommented lines 111 - 122
    --it's the storeDrawn()
    
    tried it out:
    -- because of line 120, we'll see in our console what's being pushed in the drawnArray if we draw something or hover something in the canvas
    
    problem:
    --if we change the background color of our canvas, our drawing dissapears
    
    solution:
    -uncommented the restoreCanvas() (lines 95 - 109)
    -uncommented clear canvas event listener
    -added restoreCanvas() inside the bucket color event listener
    
    trying it out 2:
    -- now if I draw with the brush and then change the bg of the canvas, it still retained mah drawing
    -- and if I press on the clear btn (the one that's red with the revert thingy, it clears mah drawing then there's a message at the top left saying 'Canvas Cleared' (that's because of the clear canvas event listener lines 87 - 93)

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ brushColorBtn.addEventListener("change", () => {
 bucketColorBtn.addEventListener("change", () => {
     bucketColor = `#${bucketColorBtn.value}`;
     createCanvas();
+    restoreCanvas();
 });
 
 // Eraser
@@ -68,8 +69,8 @@ function switchToBrush() {
     eraser.style.color = "white";
     currentColor = `#${brushColorBtn.value}`;
     currentSize = 10;
-    brushSlider.value = 10
-    displayBrushSize()
+    brushSlider.value = 10;
+    displayBrushSize();
 }
 
 // Create Canvas
@@ -82,44 +83,44 @@ function createCanvas() {
     switchToBrush();
 }
 
-// // Clear Canvas
-// clearCanvasBtn.addEventListener('click', () => {
-//   createCanvas();
-//   drawnArray = [];
-//   // Active Tool
-//   activeToolEl.textContent = 'Canvas Cleared';
-//   setTimeout(switchToBrush, 1500);
-// });
+// Clear Canvas
+clearCanvasBtn.addEventListener("click", () => {
+    createCanvas();
+    drawnArray = [];
+    // Active Tool
+    activeToolEl.textContent = "Canvas Cleared";
+    setTimeout(switchToBrush, 1500);
+});
 
-// // Draw what is stored in DrawnArray
-// function restoreCanvas() {
-//   for (let i = 1; i < drawnArray.length; i++) {
-//     context.beginPath();
-//     context.moveTo(drawnArray[i - 1].x, drawnArray[i - 1].y);
-//     context.lineWidth = drawnArray[i].size;
-//     context.lineCap = 'round';
-//     if (drawnArray[i].eraser) {
-//       context.strokeStyle = bucketColor;
-//     } else {
-//       context.strokeStyle = drawnArray[i].color;
-//     }
-//     context.lineTo(drawnArray[i].x, drawnArray[i].y);
-//     context.stroke();
-//   }
-// }
+// Draw what is stored in DrawnArray
+function restoreCanvas() {
+    for (let i = 1; i < drawnArray.length; i++) {
+        context.beginPath();
+        context.moveTo(drawnArray[i - 1].x, drawnArray[i - 1].y);
+        context.lineWidth = drawnArray[i].size;
+        context.lineCap = "round";
+        if (drawnArray[i].eraser) {
+            context.strokeStyle = bucketColor;
+        } else {
+            context.strokeStyle = drawnArray[i].color;
+        }
+        context.lineTo(drawnArray[i].x, drawnArray[i].y);
+        context.stroke();
+    }
+}
 
-// // Store Drawn Lines in DrawnArray
-// function storeDrawn(x, y, size, color, erase) {
-//   const line = {
-//     x,
-//     y,
-//     size,
-//     color,
-//     erase,
-//   };
-//   console.log(line);
-//   drawnArray.push(line);
-// }
+// Store Drawn Lines in DrawnArray
+function storeDrawn(x, y, size, color, erase) {
+    const line = {
+        x,
+        y,
+        size,
+        color,
+        erase,
+    };
+    console.log(line);
+    drawnArray.push(line);
+}
 
 // Get Mouse Position
 function getMousePosition(event) {
@@ -134,28 +135,28 @@ function getMousePosition(event) {
 canvas.addEventListener("mousedown", (event) => {
     isMouseDown = true;
     const currentPosition = getMousePosition(event);
-      context.moveTo(currentPosition.x, currentPosition.y);
-      context.beginPath();
-      context.lineWidth = currentSize;
-      context.lineCap = 'round';
-      context.strokeStyle = currentColor;
+    context.moveTo(currentPosition.x, currentPosition.y);
+    context.beginPath();
+    context.lineWidth = currentSize;
+    context.lineCap = "round";
+    context.strokeStyle = currentColor;
 });
 
 // Mouse Move
 canvas.addEventListener("mousemove", (event) => {
     if (isMouseDown) {
         const currentPosition = getMousePosition(event);
-          context.lineTo(currentPosition.x, currentPosition.y);
-          context.stroke();
-        //   storeDrawn(
-        //     currentPosition.x,
-        //     currentPosition.y,
-        //     currentSize,
-        //     currentColor,
-        //     isEraser,
-        //   );
-        // } else {
-        //   storeDrawn(undefined);
+        context.lineTo(currentPosition.x, currentPosition.y);
+        context.stroke();
+        storeDrawn(
+            currentPosition.x,
+            currentPosition.y,
+            currentSize,
+            currentColor,
+            isEraser
+        );
+    } else {
+        storeDrawn(undefined);
     }
 });
 
